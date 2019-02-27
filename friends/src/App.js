dbc685ccda24113f6c98849ea4c9acba4e62b3eb
olympiawojcik <owojcik7@gmail.com>
2019-02-27 14:59:58 -0500

    Add data from server to state using CDM and axios library

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
+//Add constructor & CDM, set friends to emtpy list
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("CDM now running");
+    //Axios is a library that helps us make HTTP cals from app to server. We need to to import axios give access to HTTP methods (get & then)
+
+    //.get() method is an HTTP method that READS from our DB, takes in a URL
+    //.then() method tells JavaScript what to do next, when the asynx has resolved successfully
+    //.catch method tells JS what to do if the asynx promise is rejected
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        console.log(res);
+        this.setState({ friends: res.data });
+      })
+      .catch(err => alert("you have CDM error:", err));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header" />
       </div>
     );
   }
